1b5d692865b45b840317e61cd7ea007c15d95d12
Afolabi Abayomi <78097841+aabyomi-786@users.noreply.github.com>
2023-06-26 20:43:30 -0400

    Update and rename test_main_Proj redo to main.cpp
    This is the redo for main.cpp after talking to partners

--- main.cpp
+++ main.cpp
@@ -17,21 +17,21 @@ int main() {
     double assignment1Grade = gradeBook.IndiGrade("Assignment 1");
     std::cout << "Assignment 1: " << assignment1Grade << std::endl;
 
-    // Get grades from a specific category and category total
+    // Gets grades from a specified category and the category total
     double assignmentsTotal = gradeBook.CategoryGrade("Assignments");
-    std::cout << "Assignments Total: " << assignmentsTotal << std::endl;
+    std::cout << "Assisgnments Total: " << assignmentsTotal << std::endl;
 
-    // Get all grades within their respective categories and a course overall
+    // Get all grades in their respective categories and a course overalls
     gradeBook.printAll();
     gradeBook.CourseGradeOption(2);
 
-    // Make changes
+    // Make the name, grade and cateory changes
     gradeBook.changeName("Assignment 1", "New Assignment");
     gradeBook.changeCategory("New Assignment", "New Category");
     gradeBook.changeGrade("New Assignment", 95);
     gradeBook.addNew("Assignment 3", "Assignments", 80);
 
-    // Save changes to the input file
+    // Save the changes to the input file which outputs all results
     gradeBook.save();
 
     return 0;
